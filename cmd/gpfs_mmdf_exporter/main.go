df03b4b94e55a9789d71c8e1e6d98110cd352866
Trey Dockendorf <tdockendorf@osc.edu>
2020-11-15 12:33:09 -0500

    Improved error handling for gpsf_mmdf_exporter

--- cmd/gpfs_mmdf_exporter/main.go
+++ cmd/gpfs_mmdf_exporter/main.go
@@ -18,12 +18,15 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
+	"sort"
+	"strings"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/gofrs/flock"
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/common/promlog/flag"
 	"github.com/prometheus/common/version"
@@ -36,61 +39,105 @@ var (
 	lockFile = kingpin.Flag("lockfile", "Lock file path").Default("/tmp/gpfs_mmdf_exporter.lock").String()
 )
 
-func collect(logger log.Logger) error {
+func writeMetrics(mfs []*dto.MetricFamily, logger log.Logger) error {
 	tmp, err := ioutil.TempFile(filepath.Dir(*output), filepath.Base(*output))
 	if err != nil {
 		level.Error(logger).Log("msg", "Unable to create temp file", "err", err)
 		return err
 	}
 	defer os.Remove(tmp.Name())
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(collectors.NewMmdfCollector(logger))
-	err = prometheus.WriteToTextfile(tmp.Name(), registry)
-	if err != nil {
-		level.Error(logger).Log("msg", "Error writing Prometheus metrics to file", "err", err)
-		return err
-	}
-	content, err := ioutil.ReadFile(tmp.Name())
-	if err != nil {
-		level.Error(logger).Log("msg", "Error reading temp file", "err", err)
-		return err
-	}
-	errorRE := regexp.MustCompile(`gpfs_exporter_collect_error{collector="mmdf-(.+)"} 1`)
-	if errorRE.Match(content) {
-		level.Error(logger).Log("msg", "Error detected with scrape")
-		if collectors.FileExists(*output) {
-			newTmp, err := ioutil.TempFile(filepath.Dir(*output), filepath.Base(*output))
-			if err != nil {
-				level.Error(logger).Log("msg", "Unable to create new temp file", "err", err)
-				return err
-			}
-			defer os.Remove(newTmp.Name())
-			oldContent, err := ioutil.ReadFile(*output)
-			if err != nil {
-				level.Error(logger).Log("msg", "Unable to read previous metrics", "err", err)
-				return err
-			}
-			match := errorRE.FindStringSubmatch(string(content))
-			newContent := string(oldContent)
-			for _, m := range match {
-				level.Debug(logger).Log("msg", "Update error metric for failed mmdf", "match", m)
-				errorRE = regexp.MustCompile(fmt.Sprintf("gpfs_exporter_collect_error{collector=\"mmdf-(%s)\"} 0", m))
-				newContent = errorRE.ReplaceAllString(newContent, "gpfs_exporter_collect_error{collector=\"mmdf-$1\"} 1")
-			}
-			level.Debug(logger).Log("msg", "Write new content", "content", newContent)
-			if _, err = newTmp.Write([]byte(newContent)); err != nil {
-				level.Error(logger).Log("msg", "Error writing to temp file", "err", err)
-				return err
-			}
-			os.Rename(newTmp.Name(), *output)
-		} else {
-			os.Rename(tmp.Name(), *output)
+	for _, mf := range mfs {
+		if _, err := expfmt.MetricFamilyToText(tmp, mf); err != nil {
+			level.Error(logger).Log("msg", "Error generating metric text", "err", err)
+			return err
 		}
-		return fmt.Errorf("Error detected with scrape")
+	}
+	if err := tmp.Close(); err != nil {
+		level.Error(logger).Log("msg", "Error closing tmp file", "err", err)
+		return err
+	}
+	if err := os.Chmod(tmp.Name(), 0644); err != nil {
+		level.Error(logger).Log("msg", "Error executing chmod 0644 on tmp file", "err", err)
+		return err
 	}
 	level.Debug(logger).Log("msg", "Renaming temp file to output", "temp", tmp.Name(), "output", *output)
-	os.Rename(tmp.Name(), *output)
+	if err := os.Rename(tmp.Name(), *output); err != nil {
+		level.Error(logger).Log("msg", "Error renaming tmp file to output", "err", err)
+		return err
+	}
 	return nil
+}
+
+func collect(logger log.Logger) error {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(collectors.NewMmdfCollector(logger))
+	var newMfs []*dto.MetricFamily
+	var failures []string
+	mfs, err := registry.Gather()
+	if err != nil {
+		level.Error(logger).Log("msg", "Error executing Gather", "err", err)
+		return err
+	}
+	for _, mf := range mfs {
+		if strings.HasPrefix(mf.GetName(), "gpfs_exporter") {
+			newMfs = append(newMfs, mf)
+		}
+		if mf.GetName() != "gpfs_exporter_collect_error" && mf.GetName() != "gpfs_exporter_collect_timeout" {
+			continue
+		}
+		for _, m := range mf.GetMetric() {
+			if m.GetGauge().GetValue() != 1 {
+				continue
+			}
+			for _, l := range m.GetLabel() {
+				if l.GetName() == "collector" && strings.HasPrefix(l.GetValue(), "mmdf-") {
+					failures = append(failures, l.GetValue())
+				}
+			}
+		}
+	}
+
+	if len(failures) != 0 && collectors.FileExists(*output) {
+		file, err := os.Open(*output)
+		if err != nil {
+			level.Error(logger).Log("msg", "Error opening metrics file", "err", err)
+			goto failure
+		}
+		parser := expfmt.TextParser{}
+		prevMfs, err := parser.TextToMetricFamilies(file)
+		file.Close()
+		if err != nil {
+			level.Error(logger).Log("msg", "Error parsing output metrics", "err", err)
+			goto failure
+		}
+		keys := make([]string, 0, len(prevMfs))
+		for k := range prevMfs {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, n := range keys {
+			mf := prevMfs[n]
+			if !strings.HasPrefix(n, "gpfs_exporter") {
+				newMfs = append(newMfs, mf)
+			}
+		}
+	} else {
+		newMfs = mfs
+	}
+
+	if err := writeMetrics(newMfs, logger); err != nil {
+		return err
+	}
+	if len(failures) != 0 {
+		return fmt.Errorf("Error with collection")
+	}
+	return nil
+
+failure:
+	if err := writeMetrics(mfs, logger); err != nil {
+		return err
+	}
+	return err
 }
 
 func main() {
