f30b6b1b6bb10c907789cb2251e6e488b66585cb
treydock <treydock@gmail.com>
2021-09-23 16:14:39 -0400

    Fix how waiters are parsed (#42)
    * Fix how waiters are parsed
    
    * Skip metrics that are unable to parse seconds

--- collectors/waiter_test.go
+++ collectors/waiter_test.go
@@ -27,49 +27,59 @@ import (
 
 var (
 	waitersStdout = `
-=== mmdiag: waiters ===
-Waiting 6861.7395 sec since 11:04:20, ignored, thread 19428 FsckClientReaperThread: on ThCond 0x7F138008EC10 (FsckReaperCondvar), reason 'Waiting to reap fsck pointer'
-Waiting 40.4231 sec since 13:08:39, monitored, thread 120656 EventsExporterSenderThread: for poll on sock 1379
-Waiting 64.3890 sec since 17:55:45, monitored, thread 120655 NSDThread: for I/O completion
-Waiting 44.3890 sec since 17:55:45, monitored, thread 120656 NSDThread: for I/O completion
-Waiting 44.3890 sec since 17:55:45, monitored, thread 120657 foobar
-Waiting 0.0409 sec since 10:24:00, monitored, thread 23170 NSDThread: for I/O completion
-Waiting 0.0259 sec since 10:24:00, monitored, thread 23241 NSDThread: for I/O completion
-Waiting 0.0251 sec since 10:24:00, monitored, thread 23243 NSDThread: for I/O completion
-Waiting 0.0173 sec since 10:24:00, monitored, thread 22893 NSDThread: for I/O completion
-Waiting 0.0158 sec since 10:24:00, monitored, thread 22933 NSDThread: for I/O completion
-Waiting 0.0153 sec since 10:24:00, monitored, thread 22953 NSDThread: for I/O completion
-Waiting 0.0143 sec since 10:24:00, monitored, thread 22978 NSDThread: for I/O completion
-Waiting 0.0139 sec since 10:24:00, monitored, thread 22996 NSDThread: for I/O completion
-Waiting 0.0128 sec since 10:24:00, monitored, thread 23025 NSDThread: for I/O completion
-Waiting 0.0121 sec since 10:24:00, monitored, thread 23047 NSDThread: for I/O completion
-Waiting 0.0114 sec since 10:24:00, monitored, thread 23075 NSDThread: for I/O completion
-Waiting 0.0109 sec since 10:24:00, monitored, thread 23097 NSDThread: for I/O completion
-Waiting 0.0099 sec since 10:24:00, monitored, thread 23130 NSDThread: for I/O completion
-Waiting 0.0069 sec since 10:24:00, monitored, thread 23337 NSDThread: for I/O completion
-Waiting 0.0063 sec since 10:24:00, monitored, thread 23227 NSDThread: for I/O completion
-Waiting 0.0039 sec since 10:24:00, monitored, thread 23267 NSDThread: for I/O completion
-Waiting 0.0023 sec since 10:24:00, monitored, thread 22922 NSDThread: for I/O completion
-Waiting 0.0022 sec since 10:24:00, monitored, thread 22931 NSDThread: for I/O completion
-Waiting 0.0002 sec since 10:24:00, monitored, thread 22987 NSDThread: for I/O completion
+mmdiag:waiters:HEADER:version:reserved:reserved:threadId:threadAddr:threadName:waitStartTime:waitTime:isMonitored:condVarAddr:condVarName:condVarReason:mutexAddr:mutexName:auxReason:delayTime:delayReason:
+mmdiag:waiters:0:1:::101445:00000000F57FC500:FsckClientReaperThread:2021-09-23_15%3A31%3A33-0400:6861.7395:monitored::::::reason 'Waiting to reap fsck pointer:::
+mmdiag:waiters:0:1:::101445:00000000F57FC500:EventsExporterSenderThread:2021-09-23_15%3A31%3A33-0400:44.3:monitored::::::for poll on sock 1379:::
+mmdiag:waiters:0:1:::101445:00000000F57FC500:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:64.3:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101445:00000000F57FC500:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:44.3:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101445:00000000F57FC500:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:44.3:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101445:00000000F57FC500:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:0.3897:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::137940:000000001401AE50:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:0.2919:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101392:00000000F57EF6C0:RebuildWorkThread:2021-09-23_15%3A31%3A33-0400:0.2234:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::127780:00000000ED808950:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1872:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::61817:000000001C02CA80:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1592:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::47037:0000000088029CD0:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1491:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::64102:0000000020097320:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1428:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::47035:0000000088029490:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1336:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::128451:0000000064004E20:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.1053:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::131854:000000006C00B3E0:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0918:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::61815:000000001C02C240:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0890:monitored:00003FF9FFD6D8C0:VdiskPGDrainCondvar:waiting for PG drain::::::
+mmdiag:waiters:0:1:::146753:00000000AC02DB90:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0696:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::47032:0000000088028830:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0547:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::128454:0000000040001C80:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0433:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101497:00000000F5808F20:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0348:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::131849:000000006001EC80:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0313:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::101482:00000000F5805980:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0298:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::47532:0000000088AFE710:NSDThread:2021-09-23_15%3A31%3A34-0400:0.0244:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::64149:00000000200A3500:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0196:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::48622:0000000088C16F10:NSDThread:2021-09-23_15%3A31%3A34-0400:0.0134:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::127779:000000003BFFFD40:RebuildWorkThread:2021-09-23_15%3A31%3A34-0400:0.0081:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::48081:0000000088B8BFB0:NSDThread:2021-09-23_15%3A31%3A34-0400:0.0037:monitored::::::for I/O completion:::
+mmdiag:waiters:0:1:::48081:0000000088B8BFB0::2021-09-23_15%3A31%3A34-0400:foo:monitored:::::::::
+foobar
+mmdiag:waiters
+mmdiag:foobar:0:1
 `
 )
 
 func TestParseMmdiagWaiters(t *testing.T) {
+	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
+		t.Fatal(err)
+	}
 	w := log.NewSyncWriter(os.Stderr)
 	logger := log.NewLogfmtLogger(w)
 	waiters := parse_mmdiag_waiters(waitersStdout, logger)
-	if val := len(waiters); val != 22 {
+	if val := len(waiters); val != 26 {
 		t.Errorf("Unexpected Waiters len got %v", val)
 		return
 	}
-	if val := waiters[0].name; val != "NSDThread" {
+	if val := waiters[0].Name; val != "RebuildWorkThread" {
 		t.Errorf("Unexpected name for waiter, got %s", val)
 	}
-	if val := waiters[0].reason; val != "for I/O completion" {
+	if val := waiters[0].Reason; val != "for I/O completion" {
 		t.Errorf("Unexpected reason for waiter, got %s", val)
 	}
-	if val := waiters[0].seconds; val != 64.3890 {
+	if val := waiters[0].Seconds; val != 64.3 {
 		t.Errorf("Unexpected seconds for waiter, got %f", val)
 	}
 }
@@ -84,18 +94,19 @@ func TestWaiterCollector(t *testing.T) {
 	expected := `
 		# HELP gpfs_waiter_seconds GPFS waiter in seconds
 		# TYPE gpfs_waiter_seconds histogram
-		gpfs_waiter_seconds_bucket{le="1"} 19
-		gpfs_waiter_seconds_bucket{le="5"} 19
-		gpfs_waiter_seconds_bucket{le="15"} 19
-		gpfs_waiter_seconds_bucket{le="60"} 21
-		gpfs_waiter_seconds_bucket{le="300"} 22
-		gpfs_waiter_seconds_bucket{le="3600"} 22
-		gpfs_waiter_seconds_bucket{le="+Inf"} 22
-		gpfs_waiter_seconds_sum 153.4144
-		gpfs_waiter_seconds_count 22
+		gpfs_waiter_seconds_bucket{le="1"} 22
+		gpfs_waiter_seconds_bucket{le="5"} 22
+		gpfs_waiter_seconds_bucket{le="15"} 22
+		gpfs_waiter_seconds_bucket{le="60"} 24
+		gpfs_waiter_seconds_bucket{le="300"} 25
+		gpfs_waiter_seconds_bucket{le="3600"} 25
+		gpfs_waiter_seconds_bucket{le="+Inf"} 25
+		gpfs_waiter_seconds_sum 155.19569999999996
+		gpfs_waiter_seconds_count 25
 		# HELP gpfs_waiter_info_count GPFS waiter info
 		# TYPE gpfs_waiter_info_count gauge
-		gpfs_waiter_info_count{waiter="NSDThread"} 21
+		gpfs_waiter_info_count{waiter="NSDThread"} 3
+		gpfs_waiter_info_count{waiter="RebuildWorkThread"} 22
 	`
 	w := log.NewSyncWriter(os.Stderr)
 	logger := log.NewLogfmtLogger(w)
@@ -105,8 +116,8 @@ func TestWaiterCollector(t *testing.T) {
 	gatherers2 := setupGatherer(collector2)
 	if val, err := testutil.GatherAndCount(gatherers1); err != nil {
 		t.Errorf("Unexpected error: %v", err)
-	} else if val != 5 {
-		t.Errorf("Unexpected collection count %d, expected 5", val)
+	} else if val != 6 {
+		t.Errorf("Unexpected collection count %d, expected 6", val)
 	}
 	if err := testutil.GatherAndCompare(gatherers2, strings.NewReader(expected),
 		"gpfs_waiter_seconds", "gpfs_waiter_info_count"); err != nil {
