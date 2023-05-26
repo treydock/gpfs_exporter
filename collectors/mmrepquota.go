4071ee4492fdce76dd92f3842a11acf031cb99e0
Evan Vokey <49615340+Evan-Vokey@users.noreply.github.com>
2023-05-26 13:40:25 +0000

    added users and groups to mmrepquota

--- collectors/mmrepquota.go
+++ collectors/mmrepquota.go
@@ -16,6 +16,7 @@ package collectors
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
@@ -29,10 +30,12 @@ import (
 
 var (
 	configMmrepquotaFilesystems = kingpin.Flag("collector.mmrepquota.filesystems", "Filesystems to query with mmrepquota, comma separated. Defaults to all filesystems.").Default("").String()
+	configMmrepquotaTypes       = kingpin.Flag("collector.mmrepquota.quotatypes", "Quota Types to query with mmrepquota, Default to fileset only").Default("j").String()
 	mmrepquotaTimeout           = kingpin.Flag("collector.mmrepquota.timeout", "Timeout for mmrepquota execution").Default("20").Int()
 	quotaMap                    = map[string]string{
 		"name":           "Name",
 		"filesystemName": "FS",
+		"quotaType":      "QuotaType",
 		"blockUsage":     "BlockUsage",
 		"blockQuota":     "BlockQuota",
 		"blockLimit":     "BlockLimit",
@@ -48,6 +51,7 @@ var (
 type QuotaMetric struct {
 	Name         string
 	FS           string
+	QuotaType    string
 	BlockUsage   float64
 	BlockQuota   float64
 	BlockLimit   float64
@@ -59,15 +63,34 @@ type QuotaMetric struct {
 }
 
 type MmrepquotaCollector struct {
-	BlockUsage   *prometheus.Desc
-	BlockQuota   *prometheus.Desc
-	BlockLimit   *prometheus.Desc
-	BlockInDoubt *prometheus.Desc
-	FilesUsage   *prometheus.Desc
-	FilesQuota   *prometheus.Desc
-	FilesLimit   *prometheus.Desc
-	FilesInDoubt *prometheus.Desc
-	logger       log.Logger
+	FilesetBlockUsage   *prometheus.Desc
+	FilesetBlockQuota   *prometheus.Desc
+	FilesetBlockLimit   *prometheus.Desc
+	FilesetBlockInDoubt *prometheus.Desc
+	FilesetFilesUsage   *prometheus.Desc
+	FilesetFilesQuota   *prometheus.Desc
+	FilesetFilesLimit   *prometheus.Desc
+	FilesetFilesInDoubt *prometheus.Desc
+
+	UserBlockUsage   *prometheus.Desc
+	UserBlockQuota   *prometheus.Desc
+	UserBlockLimit   *prometheus.Desc
+	UserBlockInDoubt *prometheus.Desc
+	UserFilesUsage   *prometheus.Desc
+	UserFilesQuota   *prometheus.Desc
+	UserFilesLimit   *prometheus.Desc
+	UserFilesInDoubt *prometheus.Desc
+
+	GroupBlockUsage   *prometheus.Desc
+	GroupBlockQuota   *prometheus.Desc
+	GroupBlockLimit   *prometheus.Desc
+	GroupBlockInDoubt *prometheus.Desc
+	GroupFilesUsage   *prometheus.Desc
+	GroupFilesQuota   *prometheus.Desc
+	GroupFilesLimit   *prometheus.Desc
+	GroupFilesInDoubt *prometheus.Desc
+
+	logger log.Logger
 }
 
 func init() {
@@ -75,37 +98,92 @@ func init() {
 }
 
 func NewMmrepquotaCollector(logger log.Logger) Collector {
-	labels := []string{"fileset", "fs"}
+	fileset_labels := []string{"fileset", "fs"}
+	user_labels := []string{"user", "fs"}
+	group_labels := []string{"group", "fs"}
 	return &MmrepquotaCollector{
-		BlockUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "used_bytes"),
-			"GPFS fileset quota used", labels, nil),
-		BlockQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "quota_bytes"),
-			"GPFS fileset block quota", labels, nil),
-		BlockLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "limit_bytes"),
-			"GPFS fileset quota block limit", labels, nil),
-		BlockInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "in_doubt_bytes"),
-			"GPFS fileset quota block in doubt", labels, nil),
-		FilesUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "used_files"),
-			"GPFS fileset quota files used", labels, nil),
-		FilesQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "quota_files"),
-			"GPFS fileset files quota", labels, nil),
-		FilesLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "limit_files"),
-			"GPFS fileset quota files limit", labels, nil),
-		FilesInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "in_doubt_files"),
-			"GPFS fileset quota files in doubt", labels, nil),
+		FilesetBlockUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "used_bytes"),
+			"GPFS fileset quota used", fileset_labels, nil),
+		FilesetBlockQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "quota_bytes"),
+			"GPFS fileset block quota", fileset_labels, nil),
+		FilesetBlockLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "limit_bytes"),
+			"GPFS fileset quota block limit", fileset_labels, nil),
+		FilesetBlockInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "in_doubt_bytes"),
+			"GPFS fileset quota block in doubt", fileset_labels, nil),
+		FilesetFilesUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "used_files"),
+			"GPFS fileset quota files used", fileset_labels, nil),
+		FilesetFilesQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "quota_files"),
+			"GPFS fileset files quota", fileset_labels, nil),
+		FilesetFilesLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "limit_files"),
+			"GPFS fileset quota files limit", fileset_labels, nil),
+		FilesetFilesInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "fileset", "in_doubt_files"),
+			"GPFS fileset quota files in doubt", fileset_labels, nil),
+
+		UserBlockUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "used_bytes"),
+			"GPFS user quota used", user_labels, nil),
+		UserBlockQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "quota_bytes"),
+			"GPFS user block quota", user_labels, nil),
+		UserBlockLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "limit_bytes"),
+			"GPFS user quota block limit", user_labels, nil),
+		UserBlockInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "in_doubt_bytes"),
+			"GPFS user quota block in doubt", user_labels, nil),
+		UserFilesUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "used_files"),
+			"GPFS user quota files used", user_labels, nil),
+		UserFilesQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "quota_files"),
+			"GPFS user files quota", user_labels, nil),
+		UserFilesLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "limit_files"),
+			"GPFS user quota files limit", user_labels, nil),
+		UserFilesInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "user", "in_doubt_files"),
+			"GPFS user quota files in doubt", user_labels, nil),
+
+		GroupBlockUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "used_bytes"),
+			"GPFS group quota used", group_labels, nil),
+		GroupBlockQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "quota_bytes"),
+			"GPFS group block quota", group_labels, nil),
+		GroupBlockLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "limit_bytes"),
+			"GPFS group quota block limit", group_labels, nil),
+		GroupBlockInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "in_doubt_bytes"),
+			"GPFS group quota block in doubt", group_labels, nil),
+		GroupFilesUsage: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "used_files"),
+			"GPFS group quota files used", group_labels, nil),
+		GroupFilesQuota: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "quota_files"),
+			"GPFS group files quota", group_labels, nil),
+		GroupFilesLimit: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "limit_files"),
+			"GPFS group quota files limit", group_labels, nil),
+		GroupFilesInDoubt: prometheus.NewDesc(prometheus.BuildFQName(namespace, "group", "in_doubt_files"),
+			"GPFS group quota files in doubt", group_labels, nil),
+
 		logger: logger,
 	}
 }
 
 func (c *MmrepquotaCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.BlockUsage
-	ch <- c.BlockQuota
-	ch <- c.BlockLimit
-	ch <- c.BlockInDoubt
-	ch <- c.FilesUsage
-	ch <- c.FilesQuota
-	ch <- c.FilesLimit
-	ch <- c.FilesInDoubt
+	ch <- c.FilesetBlockUsage
+	ch <- c.FilesetBlockQuota
+	ch <- c.FilesetBlockLimit
+	ch <- c.FilesetBlockInDoubt
+	ch <- c.FilesetFilesUsage
+	ch <- c.FilesetFilesQuota
+	ch <- c.FilesetFilesLimit
+	ch <- c.FilesetFilesInDoubt
+
+	ch <- c.UserBlockUsage
+	ch <- c.UserBlockQuota
+	ch <- c.UserBlockLimit
+	ch <- c.UserBlockInDoubt
+	ch <- c.UserFilesUsage
+	ch <- c.UserFilesQuota
+	ch <- c.UserFilesLimit
+	ch <- c.UserFilesInDoubt
+
+	ch <- c.GroupBlockUsage
+	ch <- c.GroupBlockQuota
+	ch <- c.GroupBlockLimit
+	ch <- c.GroupBlockInDoubt
+	ch <- c.GroupFilesUsage
+	ch <- c.GroupFilesQuota
+	ch <- c.GroupFilesLimit
+	ch <- c.GroupFilesInDoubt
 }
 
 func (c *MmrepquotaCollector) Collect(ch chan<- prometheus.Metric) {
@@ -113,34 +191,60 @@ func (c *MmrepquotaCollector) Collect(ch chan<- prometheus.Metric) {
 	collectTime := time.Now()
 	timeout := 0
 	errorMetric := 0
-	metrics, err := c.collect()
-	if err == context.DeadlineExceeded {
-		timeout = 1
-		level.Error(c.logger).Log("msg", "Timeout executing mmrepquota")
-	} else if err != nil {
-		level.Error(c.logger).Log("msg", err)
-		errorMetric = 1
+	metrics := []QuotaMetric{}
+
+	for _, quotaType := range *configMmrepquotaTypes {
+		metric, err := c.collect(fmt.Sprintf("-%c", quotaType))
+		metrics = append(metrics, metric...)
+
+		if err == context.DeadlineExceeded {
+			timeout = 1
+			level.Error(c.logger).Log("msg", "Timeout executing mmrepquota")
+		} else if err != nil {
+			level.Error(c.logger).Log("msg", err)
+			errorMetric = 1
+		}
 	}
 
 	for _, m := range metrics {
-		ch <- prometheus.MustNewConstMetric(c.BlockUsage, prometheus.GaugeValue, m.BlockUsage, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.BlockQuota, prometheus.GaugeValue, m.BlockQuota, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.BlockLimit, prometheus.GaugeValue, m.BlockLimit, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.BlockInDoubt, prometheus.GaugeValue, m.BlockInDoubt, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.FilesUsage, prometheus.GaugeValue, m.FilesUsage, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.FilesQuota, prometheus.GaugeValue, m.FilesQuota, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.FilesLimit, prometheus.GaugeValue, m.FilesLimit, m.Name, m.FS)
-		ch <- prometheus.MustNewConstMetric(c.FilesInDoubt, prometheus.GaugeValue, m.FilesInDoubt, m.Name, m.FS)
+		if m.QuotaType == "FILESET" {
+			ch <- prometheus.MustNewConstMetric(c.FilesetBlockUsage, prometheus.GaugeValue, m.BlockUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetBlockQuota, prometheus.GaugeValue, m.BlockQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetBlockLimit, prometheus.GaugeValue, m.BlockLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetBlockInDoubt, prometheus.GaugeValue, m.BlockInDoubt, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetFilesUsage, prometheus.GaugeValue, m.FilesUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetFilesQuota, prometheus.GaugeValue, m.FilesQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetFilesLimit, prometheus.GaugeValue, m.FilesLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.FilesetFilesInDoubt, prometheus.GaugeValue, m.FilesInDoubt, m.Name, m.FS)
+		} else if m.QuotaType == "USR" {
+			ch <- prometheus.MustNewConstMetric(c.UserBlockUsage, prometheus.GaugeValue, m.BlockUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserBlockQuota, prometheus.GaugeValue, m.BlockQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserBlockLimit, prometheus.GaugeValue, m.BlockLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserBlockInDoubt, prometheus.GaugeValue, m.BlockInDoubt, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserFilesUsage, prometheus.GaugeValue, m.FilesUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserFilesQuota, prometheus.GaugeValue, m.FilesQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserFilesLimit, prometheus.GaugeValue, m.FilesLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.UserFilesInDoubt, prometheus.GaugeValue, m.FilesInDoubt, m.Name, m.FS)
+		} else if m.QuotaType == "GRP" {
+			ch <- prometheus.MustNewConstMetric(c.GroupBlockUsage, prometheus.GaugeValue, m.BlockUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupBlockQuota, prometheus.GaugeValue, m.BlockQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupBlockLimit, prometheus.GaugeValue, m.BlockLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupBlockInDoubt, prometheus.GaugeValue, m.BlockInDoubt, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupFilesUsage, prometheus.GaugeValue, m.FilesUsage, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupFilesQuota, prometheus.GaugeValue, m.FilesQuota, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupFilesLimit, prometheus.GaugeValue, m.FilesLimit, m.Name, m.FS)
+			ch <- prometheus.MustNewConstMetric(c.GroupFilesInDoubt, prometheus.GaugeValue, m.FilesInDoubt, m.Name, m.FS)
+		}
 	}
 	ch <- prometheus.MustNewConstMetric(collectError, prometheus.GaugeValue, float64(errorMetric), "mmrepquota")
 	ch <- prometheus.MustNewConstMetric(collecTimeout, prometheus.GaugeValue, float64(timeout), "mmrepquota")
 	ch <- prometheus.MustNewConstMetric(collectDuration, prometheus.GaugeValue, time.Since(collectTime).Seconds(), "mmrepquota")
 }
 
-func (c *MmrepquotaCollector) collect() ([]QuotaMetric, error) {
+func (c *MmrepquotaCollector) collect(typeArg string) ([]QuotaMetric, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*mmrepquotaTimeout)*time.Second)
 	defer cancel()
-	out, err := mmrepquotaExec(ctx)
+	out, err := mmrepquotaExec(ctx, typeArg)
 	if err != nil {
 		return nil, err
 	}
@@ -148,13 +252,15 @@ func (c *MmrepquotaCollector) collect() ([]QuotaMetric, error) {
 	return metric, nil
 }
 
-func mmrepquota(ctx context.Context) (string, error) {
-	args := []string{"/usr/lpp/mmfs/bin/mmrepquota", "-j", "-Y"}
+func mmrepquota(ctx context.Context, typeArg string) (string, error) {
+	args := []string{"/usr/lpp/mmfs/bin/mmrepquota", typeArg, "-Y"}
+
 	if *configMmrepquotaFilesystems == "" {
 		args = append(args, "-a")
 	} else {
 		args = append(args, strings.Split(*configMmrepquotaFilesystems, ",")...)
 	}
+
 	cmd := execCommand(ctx, *sudoCmd, args...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
