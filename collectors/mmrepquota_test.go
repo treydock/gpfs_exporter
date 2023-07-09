7d13cd17656d482e11d279d01f00493f048694f6
Evan Vokey <49615340+Evan-Vokey@users.noreply.github.com>
2023-07-09 14:21:32 -0230

    Add users and groups mmrepquota (#59)
    * added users and groups to mmrepquota
    
    * modified tests for mmrepquota to include users and groups
    
    * added documentation to README.md for collector.mmrepquota.quotatypes flag
    
    Co-authored-by: treydock <treydock@gmail.com>

--- collectors/mmrepquota_test.go
+++ collectors/mmrepquota_test.go
@@ -36,6 +36,30 @@ mmrepquota::0:1:::project:FILESET:408:PZS1003:341467872:2147483648:2147483648:0:
 mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
 mmrepquota::0:1:::scratch:FILESET:0:root:928235294208:0:0:5308909920:none:141909093:0:0:140497:none:i:on:off:::
 `
+
+	mmrepquotaStdoutAll = `
+*** Report for FILESET quotas on project
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::project:FILESET:0:root:337419744:0:0:163840:none:1395:0:0:400:none:i:on:off:::
+mmrepquota::0:1:::project:FILESET:408:PZS1003:341467872:2147483648:2147483648:0:none:6286:2000000:2000000:0:none:e:on:off:::
+*** Report for FILESET quotas on scratch
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::scratch:FILESET:0:root:928235294208:0:0:5308909920:none:141909093:0:0:140497:none:i:on:off:::
+*** Report for USR quotas on home
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::home:USR:0:root:337419744:0:0:163840:none:1395:0:0:400:none:i:on:off:::
+mmrepquota::0:1:::home:USR:408:PZS1003:341467872:2147483648:2147483648:0:none:6286:2000000:2000000:0:none:e:on:off:::
+*** Report for USR quotas on scratch
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::scratch:USR:0:root:928235294208:0:0:5308909920:none:141909093:0:0:140497:none:i:on:off:::
+*** Report for GRP quotas on project
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::project:GRP:0:root:337419744:0:0:163840:none:1395:0:0:400:none:i:on:off:::
+mmrepquota::0:1:::project:GRP:408:PZS1003:341467872:2147483648:2147483648:0:none:6286:2000000:2000000:0:none:e:on:off:::
+*** Report for GRP quotas on scratch
+mmrepquota::HEADER:version:reserved:reserved:filesystemName:quotaType:id:name:blockUsage:blockQuota:blockLimit:blockInDoubt:blockGrace:filesUsage:filesQuota:filesLimit:filesInDoubt:filesGrace:remarks:quota:defQuota:fid:filesetname:
+mmrepquota::0:1:::scratch:GRP:0:root:928235294208:0:0:5308909920:none:141909093:0:0:140497:none:i:on:off:::
+`
 )
 
 func TestMmrepquota(t *testing.T) {
@@ -45,7 +69,7 @@ func TestMmrepquota(t *testing.T) {
 	defer func() { execCommand = exec.CommandContext }()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	out, err := mmrepquota(ctx)
+	out, err := mmrepquota(ctx, "-j")
 	if err != nil {
 		t.Errorf("Unexpected error: %s", err.Error())
 	}
@@ -61,7 +85,7 @@ func TestMmrepquotaError(t *testing.T) {
 	defer func() { execCommand = exec.CommandContext }()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	out, err := mmrepquota(ctx)
+	out, err := mmrepquota(ctx, "-j")
 	if err == nil {
 		t.Errorf("Expected error")
 	}
@@ -77,7 +101,7 @@ func TestMmrepquotaTimeout(t *testing.T) {
 	defer func() { execCommand = exec.CommandContext }()
 	ctx, cancel := context.WithTimeout(context.Background(), 0*time.Second)
 	defer cancel()
-	out, err := mmrepquota(ctx)
+	out, err := mmrepquota(ctx, "-j")
 	if err != context.DeadlineExceeded {
 		t.Errorf("Expected DeadlineExceeded")
 	}
@@ -105,12 +129,31 @@ func TestParseMmrepquota(t *testing.T) {
 		t.Errorf("Unexpected BlockInDoubt got %v", val)
 	}
 }
+func TestParseMmrepquotaAll(t *testing.T) {
+	metrics := parse_mmrepquota(mmrepquotaStdoutAll, log.NewNopLogger())
+	if len(metrics) != 9 {
+		t.Errorf("Unexpected metric count: %d", len(metrics))
+		return
+	}
+	if val := metrics[0].BlockUsage; val != 345517817856 {
+		t.Errorf("Unexpected BlockUsage got %v", val)
+	}
+	if val := metrics[0].BlockQuota; val != 0 {
+		t.Errorf("Unexpected BlockQuota got %v", val)
+	}
+	if val := metrics[0].BlockLimit; val != 0 {
+		t.Errorf("Unexpected BlockLimit got %v", val)
+	}
+	if val := metrics[0].BlockInDoubt; val != 167772160 {
+		t.Errorf("Unexpected BlockInDoubt got %v", val)
+	}
+}
 
 func TestMmrepquotaCollector(t *testing.T) {
 	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
 		t.Fatal(err)
 	}
-	mmrepquotaExec = func(ctx context.Context) (string, error) {
+	mmrepquotaExec = func(ctx context.Context, typeArg string) (string, error) {
 		return mmrepquotaStdout, nil
 	}
 	expected := `
@@ -120,6 +163,7 @@ gpfs_exporter_collect_error{collector="mmrepquota"} 0
 # HELP gpfs_exporter_collect_timeout Indicates the collector timed out
 # TYPE gpfs_exporter_collect_timeout gauge
 gpfs_exporter_collect_timeout{collector="mmrepquota"} 0
+
 # HELP gpfs_fileset_in_doubt_bytes GPFS fileset quota block in doubt
 # TYPE gpfs_fileset_in_doubt_bytes gauge
 gpfs_fileset_in_doubt_bytes{fileset="PZS1003",fs="project"} 0
@@ -160,7 +204,8 @@ gpfs_fileset_used_bytes{fileset="root",fs="scratch"} 950512941268992
 gpfs_fileset_used_files{fileset="PZS1003",fs="project"} 6286
 gpfs_fileset_used_files{fileset="root",fs="project"} 1395
 gpfs_fileset_used_files{fileset="root",fs="scratch"} 141909093
-	`
+`
+
 	collector := NewMmrepquotaCollector(log.NewNopLogger())
 	gatherers := setupGatherer(collector)
 	if val, err := testutil.GatherAndCount(gatherers); err != nil {
@@ -170,6 +215,7 @@ gpfs_fileset_used_files{fileset="root",fs="scratch"} 141909093
 	}
 	if err := testutil.GatherAndCompare(gatherers, strings.NewReader(expected),
 		"gpfs_exporter_collect_error", "gpfs_exporter_collect_timeout",
+
 		"gpfs_fileset_in_doubt_bytes", "gpfs_fileset_in_doubt_files",
 		"gpfs_fileset_limit_bytes", "gpfs_fileset_limit_files",
 		"gpfs_fileset_quota_bytes", "gpfs_fileset_quota_files",
@@ -178,11 +224,177 @@ gpfs_fileset_used_files{fileset="root",fs="scratch"} 141909093
 	}
 }
 
+func TestMmrepquotaCollectorAll(t *testing.T) {
+	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
+		t.Fatal(err)
+	}
+	mmrepquotaExec = func(ctx context.Context, typeArg string) (string, error) {
+		return mmrepquotaStdoutAll, nil
+	}
+	expected := `
+# HELP gpfs_exporter_collect_error Indicates if error has occurred during collection
+# TYPE gpfs_exporter_collect_error gauge
+gpfs_exporter_collect_error{collector="mmrepquota"} 0
+# HELP gpfs_exporter_collect_timeout Indicates the collector timed out
+# TYPE gpfs_exporter_collect_timeout gauge
+gpfs_exporter_collect_timeout{collector="mmrepquota"} 0
+
+# HELP gpfs_fileset_in_doubt_bytes GPFS fileset quota block in doubt
+# TYPE gpfs_fileset_in_doubt_bytes gauge
+gpfs_fileset_in_doubt_bytes{fileset="PZS1003",fs="project"} 0
+gpfs_fileset_in_doubt_bytes{fileset="root",fs="project"} 167772160
+gpfs_fileset_in_doubt_bytes{fileset="root",fs="scratch"} 5436323758080
+# HELP gpfs_fileset_in_doubt_files GPFS fileset quota files in doubt
+# TYPE gpfs_fileset_in_doubt_files gauge
+gpfs_fileset_in_doubt_files{fileset="PZS1003",fs="project"} 0
+gpfs_fileset_in_doubt_files{fileset="root",fs="project"} 400
+gpfs_fileset_in_doubt_files{fileset="root",fs="scratch"} 140497
+# HELP gpfs_fileset_limit_bytes GPFS fileset quota block limit
+# TYPE gpfs_fileset_limit_bytes gauge
+gpfs_fileset_limit_bytes{fileset="PZS1003",fs="project"} 2199023255552
+gpfs_fileset_limit_bytes{fileset="root",fs="project"} 0
+gpfs_fileset_limit_bytes{fileset="root",fs="scratch"} 0
+# HELP gpfs_fileset_limit_files GPFS fileset quota files limit
+# TYPE gpfs_fileset_limit_files gauge
+gpfs_fileset_limit_files{fileset="PZS1003",fs="project"} 2000000
+gpfs_fileset_limit_files{fileset="root",fs="project"} 0
+gpfs_fileset_limit_files{fileset="root",fs="scratch"} 0
+# HELP gpfs_fileset_quota_bytes GPFS fileset block quota
+# TYPE gpfs_fileset_quota_bytes gauge
+gpfs_fileset_quota_bytes{fileset="PZS1003",fs="project"} 2199023255552
+gpfs_fileset_quota_bytes{fileset="root",fs="project"} 0
+gpfs_fileset_quota_bytes{fileset="root",fs="scratch"} 0
+# HELP gpfs_fileset_quota_files GPFS fileset files quota
+# TYPE gpfs_fileset_quota_files gauge
+gpfs_fileset_quota_files{fileset="PZS1003",fs="project"} 2000000
+gpfs_fileset_quota_files{fileset="root",fs="project"} 0
+gpfs_fileset_quota_files{fileset="root",fs="scratch"} 0
+# HELP gpfs_fileset_used_bytes GPFS fileset quota used
+# TYPE gpfs_fileset_used_bytes gauge
+gpfs_fileset_used_bytes{fileset="PZS1003",fs="project"} 349663100928
+gpfs_fileset_used_bytes{fileset="root",fs="project"} 345517817856
+gpfs_fileset_used_bytes{fileset="root",fs="scratch"} 950512941268992
+# HELP gpfs_fileset_used_files GPFS fileset quota files used
+# TYPE gpfs_fileset_used_files gauge
+gpfs_fileset_used_files{fileset="PZS1003",fs="project"} 6286
+gpfs_fileset_used_files{fileset="root",fs="project"} 1395
+gpfs_fileset_used_files{fileset="root",fs="scratch"} 141909093
+# HELP gpfs_user_in_doubt_bytes GPFS user quota block in doubt
+# TYPE gpfs_user_in_doubt_bytes gauge
+gpfs_user_in_doubt_bytes{fs="home",user="PZS1003"} 0
+gpfs_user_in_doubt_bytes{fs="home",user="root"} 167772160
+gpfs_user_in_doubt_bytes{fs="scratch",user="root"} 5436323758080
+# HELP gpfs_user_in_doubt_files GPFS user quota files in doubt
+# TYPE gpfs_user_in_doubt_files gauge
+gpfs_user_in_doubt_files{fs="home",user="PZS1003"} 0
+gpfs_user_in_doubt_files{fs="home",user="root"} 400
+gpfs_user_in_doubt_files{fs="scratch",user="root"} 140497
+# HELP gpfs_user_limit_bytes GPFS user quota block limit
+# TYPE gpfs_user_limit_bytes gauge
+gpfs_user_limit_bytes{fs="home",user="PZS1003"} 2199023255552
+gpfs_user_limit_bytes{fs="home",user="root"} 0
+gpfs_user_limit_bytes{fs="scratch",user="root"} 0
+# HELP gpfs_user_limit_files GPFS user quota files limit
+# TYPE gpfs_user_limit_files gauge
+gpfs_user_limit_files{fs="home",user="PZS1003"} 2000000
+gpfs_user_limit_files{fs="home",user="root"} 0
+gpfs_user_limit_files{fs="scratch",user="root"} 0
+# HELP gpfs_user_quota_bytes GPFS user block quota
+# TYPE gpfs_user_quota_bytes gauge
+gpfs_user_quota_bytes{fs="home",user="PZS1003"} 2199023255552
+gpfs_user_quota_bytes{fs="home",user="root"} 0
+gpfs_user_quota_bytes{fs="scratch",user="root"} 0
+# HELP gpfs_user_quota_files GPFS user files quota
+# TYPE gpfs_user_quota_files gauge
+gpfs_user_quota_files{fs="home",user="PZS1003"} 2000000
+gpfs_user_quota_files{fs="home",user="root"} 0
+gpfs_user_quota_files{fs="scratch",user="root"} 0
+# HELP gpfs_user_used_bytes GPFS user quota used
+# TYPE gpfs_user_used_bytes gauge
+gpfs_user_used_bytes{fs="home",user="PZS1003"} 349663100928
+gpfs_user_used_bytes{fs="home",user="root"} 345517817856
+gpfs_user_used_bytes{fs="scratch",user="root"} 950512941268992
+# HELP gpfs_user_used_files GPFS user quota files used
+# TYPE gpfs_user_used_files gauge
+gpfs_user_used_files{fs="home",user="PZS1003"} 6286
+gpfs_user_used_files{fs="home",user="root"} 1395
+gpfs_user_used_files{fs="scratch",user="root"} 141909093
+
+# HELP gpfs_group_in_doubt_bytes GPFS group quota block in doubt
+# TYPE gpfs_group_in_doubt_bytes gauge
+gpfs_group_in_doubt_bytes{fs="project",group="PZS1003"} 0
+gpfs_group_in_doubt_bytes{fs="project",group="root"} 167772160
+gpfs_group_in_doubt_bytes{fs="scratch",group="root"} 5436323758080
+# HELP gpfs_group_in_doubt_files GPFS group quota files in doubt
+# TYPE gpfs_group_in_doubt_files gauge
+gpfs_group_in_doubt_files{fs="project",group="PZS1003"} 0
+gpfs_group_in_doubt_files{fs="project",group="root"} 400
+gpfs_group_in_doubt_files{fs="scratch",group="root"} 140497
+# HELP gpfs_group_limit_bytes GPFS group quota block limit
+# TYPE gpfs_group_limit_bytes gauge
+gpfs_group_limit_bytes{fs="project",group="PZS1003"} 2199023255552
+gpfs_group_limit_bytes{fs="project",group="root"} 0
+gpfs_group_limit_bytes{fs="scratch",group="root"} 0
+# HELP gpfs_group_limit_files GPFS group quota files limit
+# TYPE gpfs_group_limit_files gauge
+gpfs_group_limit_files{fs="project",group="PZS1003"} 2000000
+gpfs_group_limit_files{fs="project",group="root"} 0
+gpfs_group_limit_files{fs="scratch",group="root"} 0
+# HELP gpfs_group_quota_bytes GPFS group block quota
+# TYPE gpfs_group_quota_bytes gauge
+gpfs_group_quota_bytes{fs="project",group="PZS1003"} 2199023255552
+gpfs_group_quota_bytes{fs="project",group="root"} 0
+gpfs_group_quota_bytes{fs="scratch",group="root"} 0
+# HELP gpfs_group_quota_files GPFS group files quota
+# TYPE gpfs_group_quota_files gauge
+gpfs_group_quota_files{fs="project",group="PZS1003"} 2000000
+gpfs_group_quota_files{fs="project",group="root"} 0
+gpfs_group_quota_files{fs="scratch",group="root"} 0
+# HELP gpfs_group_used_bytes GPFS group quota used
+# TYPE gpfs_group_used_bytes gauge
+gpfs_group_used_bytes{fs="project",group="PZS1003"} 349663100928
+gpfs_group_used_bytes{fs="project",group="root"} 345517817856
+gpfs_group_used_bytes{fs="scratch",group="root"} 950512941268992
+# HELP gpfs_group_used_files GPFS group quota files used
+# TYPE gpfs_group_used_files gauge
+gpfs_group_used_files{fs="project",group="PZS1003"} 6286
+gpfs_group_used_files{fs="project",group="root"} 1395
+gpfs_group_used_files{fs="scratch",group="root"} 141909093
+`
+
+	collector := NewMmrepquotaCollector(log.NewNopLogger())
+	gatherers := setupGatherer(collector)
+	if val, err := testutil.GatherAndCount(gatherers); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else if val != 75 {
+		t.Errorf("Unexpected collection count %d, expected 75", val)
+	}
+	if err := testutil.GatherAndCompare(gatherers, strings.NewReader(expected),
+		"gpfs_exporter_collect_error", "gpfs_exporter_collect_timeout",
+
+		"gpfs_fileset_in_doubt_bytes", "gpfs_fileset_in_doubt_files",
+		"gpfs_fileset_limit_bytes", "gpfs_fileset_limit_files",
+		"gpfs_fileset_quota_bytes", "gpfs_fileset_quota_files",
+		"gpfs_fileset_used_bytes", "gpfs_fileset_used_files",
+
+		"gpfs_user_in_doubt_bytes", "gpfs_user_in_doubt_files",
+		"gpfs_user_limit_bytes", "gpfs_user_limit_files",
+		"gpfs_user_quota_bytes", "gpfs_user_quota_files",
+		"gpfs_user_used_bytes", "gpfs_user_used_files",
+
+		"gpfs_group_in_doubt_bytes", "gpfs_group_in_doubt_files",
+		"gpfs_group_limit_bytes", "gpfs_group_limit_files",
+		"gpfs_group_quota_bytes", "gpfs_group_quota_files",
+		"gpfs_group_used_bytes", "gpfs_group_used_files"); err != nil {
+		t.Errorf("unexpected collecting result:\n%s", err)
+	}
+}
+
 func TestMMrepquotaCollectorError(t *testing.T) {
 	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
 		t.Fatal(err)
 	}
-	mmrepquotaExec = func(ctx context.Context) (string, error) {
+	mmrepquotaExec = func(ctx context.Context, typeArg string) (string, error) {
 		return "", fmt.Errorf("Error")
 	}
 	expected := `
@@ -207,7 +419,7 @@ func TestMMrepquotaCollectorTimeout(t *testing.T) {
 	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
 		t.Fatal(err)
 	}
-	mmrepquotaExec = func(ctx context.Context) (string, error) {
+	mmrepquotaExec = func(ctx context.Context, typeArg string) (string, error) {
 		return "", context.DeadlineExceeded
 	}
 	expected := `
